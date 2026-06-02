b124a502a3cb6dd7853e7efc6ed329bca7f8ca8f
Divyabharathi R <23csa34@karpagamtech.ac.in>
2026-06-02 20:45:52 +0530

    Implement sum of even digits in a number
    Calculate the sum of even digits in a number.

---
+++ addingevendigits.java
@@ -0,0 +1,20 @@
+public class Main
+{
+	public static void main(String[] args) {
+		int n=1234;
+		int sum =0;
+	while(n!=0){
+	    int d = n%10;
+	    n=n/10;
+	
+	    if(d%2==0){
+	        sum =sum+d;
+	    
+	         
+	    
+	    }
+	}
+	System.out.println(sum);
+	
+	}
+}
