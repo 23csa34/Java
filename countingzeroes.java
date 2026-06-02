02831f72c2102437060267ad8c7c3fb6d0bc188b
Divyabharathi R <23csa34@karpagamtech.ac.in>
2026-06-02 21:19:43 +0530

    Add zero counting logic in countingzeroes.java
    Count the number of zeroes in the integer 6000.

---
+++ countingzeroes.java
@@ -0,0 +1,21 @@
+public class Main
+{
+	public static void main(String[] args) {
+		int n=6000;
+		int count =0;
+	while(n!=0){
+	    int d = n%10;
+	    n=n/10;
+	
+	    if(d%10==0){
+	         count++;
+	        
+	    
+	         
+	    
+	    }
+	}
+	System.out.println(count);
+	
+	}
+}
