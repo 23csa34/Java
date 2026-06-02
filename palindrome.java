7b1bceaa089b04ab03730d2c22f8eec41d17000a
Divyabharathi R <23csa34@karpagamtech.ac.in>
2026-06-02 10:09:42 +0530

    Implement palindrome check in palindrome.java
    Fix palindrome check logic and output.

---
+++ palindrome.java
@@ -0,0 +1,20 @@
+
+public class Main{
+    public static void main(String args[]){
+        int n = 1234;
+        int rev =0;
+        while(n!=0){
+            int d =n%10;
+            rev=rev*10+d;
+            n=n/10;
+        }
+         if(n==rev){
+             System.out.print("palindrome");
+         }  
+         else{
+             System.out.print("Not palindrome");
+         }
+        
+        
+    }
+}
