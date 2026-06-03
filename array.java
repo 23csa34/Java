b1c8dddf819e43a20cebc9233ee5c49919a4d9ca
Divyabharathi R <23csa34@karpagamtech.ac.in>
2026-06-03 09:44:28 +0530

    Create array.java

---
+++ array.java
@@ -0,0 +1,10 @@
+public class Main
+{
+	public static void main(String[] args) {
+	    int arr[]= new int[5];
+	    for(int i =0;i<5;i++){
+	        System.out.print(arr[i]+" ");
+	    }
+	    
+	}
+}
